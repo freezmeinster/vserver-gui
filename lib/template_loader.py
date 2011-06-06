b53390b3b3b4ce16aa2997dfde48440bfaa431e2
Bramandityo Prabowo <labtek@labtek14.(none)>
2011-06-06 12:50:39 +0700

    bikin template loader

---
+++ lib/template_loader.py
@@ -0,0 +1,8 @@
+from jinja2 import Template, Environment, FileSystemLoader
+import os.path
+
+def render(template_name, data=''):
+	path = os.path.join(os.path.dirname(__file__), '../templates')
+	env = Environment(loader=FileSystemLoader(path))
+	tmpl = env.get_template(template_name)
+	return tmpl.render(data)
