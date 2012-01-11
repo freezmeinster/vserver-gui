a7d731acd3b9e768b2cd3e17ffd3c1a5d31ac9af
Bramandityo Prabowo <freez_meinster@yahoo.co.id>
2012-01-11 08:55:01 +0000

    rancu bagian pyro nya

--- lib/remote/master.py
+++ lib/remote/master.py
@@ -1,6 +1,11 @@
-import Pyro4
+import Pyro4,hmac
 
-info = Pyro4.Proxy("PYRO:VpsInfo@192.168.70.1:4000")
+info = Pyro4.Proxy("PYRO:VpsServer@192.168.70.1:4000")
+
+key = "IlkomUpi"
+hmac_key = hmac.new(key)
+Pyro4.config.HMAC_KEY = hmac_key.hexdigest()
 
 def VpsList():
-    return info.getVpsList()
+    vps = info.vpsserver()
+    return vps.get_registered_vps()
